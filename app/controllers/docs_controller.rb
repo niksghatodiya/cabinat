f0f7f9ed024c9661ec7ec8caba073f133e0a09bd
nikhil ghatodiya <neck.nikhil@gmail.com>
2023-03-06 16:18:14 +0530

    testing

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,16 +1,29 @@
 class DocsController < ApplicationController
     def index
     end
+
     def show
     end
+
     def new
+        @doc= Doc.new()
     end
+
     def create
+        @doc=Doc.new(doc_params)
+        if @doc.save
+            redirect_to @doc
+        else
+            render 'new'
+        end
     end
+
     def edit
     end
+
     def update
     end
+
     def destroy
     end
 
@@ -19,5 +32,6 @@ class DocsController < ApplicationController
     def find_doc
     end
     def doc_params
+        params.require(:doc).permit(:title,:content)
     end
 end
